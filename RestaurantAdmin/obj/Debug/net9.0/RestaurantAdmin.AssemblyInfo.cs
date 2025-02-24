300c1df04ebc75c37df9a8d61b24c52407cf9231
Shivankit Agarwal <shivankit1210@gmail.com>
2025-02-24 10:54:39 +0530

    ADDED delete button

--- RestaurantAdmin/obj/Debug/net9.0/RestaurantAdmin.AssemblyInfo.cs
+++ RestaurantAdmin/obj/Debug/net9.0/RestaurantAdmin.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RestaurantAdmin")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+289094a2c85b9aeb6778d843ade7fa7a25a2bf6a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+110b96ccc4b78c51203bc07c51e30dd5376ed440")]
 [assembly: System.Reflection.AssemblyProductAttribute("RestaurantAdmin")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RestaurantAdmin")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
